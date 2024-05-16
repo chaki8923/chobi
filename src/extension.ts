daa07bde6802a118195ab2703d53f4856fdf9562
茶木 涼 <>
2024-05-16 18:22:35 +0900

    アルティメット

--- src/extension.ts
+++ src/extension.ts
@@ -53,13 +53,37 @@ export function activate(context: vscode.ExtensionContext) {
       vscode.window.showInformationMessage("Please wating.....");
       // rspecコマンドを実行する
       exec(
-        `docker exec ibjs_api bash -c "cd api && rspec ${apiBasePath}${currentFileName}:${currentLineNumber}"`, (error,stdout,stderr) => {
-          if(!error){
-            vscode.window.showInformationMessage(`"Test All Passed!!${stdout}"`);
-          }else{
-            vscode.window.showErrorMessage(`"Test failure => ${stdout}"`);
+        `docker exec ibjs_api bash -c "cd api && rspec ${apiBasePath}${currentFileName}:${currentLineNumber}"`,
+        (error, stdout, stderr) => {
+          const match = stdout.match(/(\d+) examples?, (\d+) failures?/);
+          const examples = parseInt(match![1]);
+          const failures = parseInt(match![2]);
+          const success = examples - failures;
+          if (!error) {
+            vscode.window.showInformationMessage(`
+            ALL PASSED🤩\n実行したテスト=>「 ${examples} 」|
+             成功「 ${success} 」|
+             失敗 「 ${failures} 」`);
+          } else {
+            const panel = vscode.window.createWebviewPanel(
+              "testResults", // Identifies the type of the webview. Used internally
+              "Test Results", // Title of the panel displayed to the user
+              vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+              {}, // Webview options. More on these later.
+            );
+            panel.webview.html = getFailContent(
+              stderr,
+              examples,
+              success,
+              failures,
+              currentFileName,
+            );
+            // vscode.window.showErrorMessage(
+            //   `ぴえん🥲 実行したテスト=>「 ${examples} 」 | 成功「 ${success} 」| 失敗 「 ${failures} 」| メッセージ・・・・>${stdout}`,
+            // );
           }
-        });
+        },
+      );
     } catch (error: unknown) {
       if (error instanceof Error) {
         vscode.window.showErrorMessage(`MochobiError👎: ${error.stack}`);
@@ -67,29 +91,32 @@ export function activate(context: vscode.ExtensionContext) {
     }
   });
 
-  let controller_or_model_jump = vscode.commands.registerCommand("ojump", () => {
-    let editor = vscode.window.activeTextEditor;
+  let controller_or_model_jump = vscode.commands.registerCommand(
+    "ojump",
+    () => {
+      let editor = vscode.window.activeTextEditor;
 
-    if (editor === undefined) {
-      return;
-    }
-    const currentFilePath = editor.document.fileName;
-    const currentFileName = path.basename(currentFilePath);
-    const jumpFileName = currentFileName.replace("_spec", "");
-    let fileName = ""
-    if(currentFileName.includes("controller")){
-      fileName = `apps/api/app/controllers/${jumpFileName}`; // アクティブにしたいファイルの名前
-    }else{
-      fileName = `apps/api/app/models/${jumpFileName}`; // アクティブにしたいファイルの名前
-    }
+      if (editor === undefined) {
+        return;
+      }
+      const currentFilePath = editor.document.fileName;
+      const currentFileName = path.basename(currentFilePath);
+      const jumpFileName = currentFileName.replace("_spec", "");
+      let fileName = "";
+      if (currentFileName.includes("controller")) {
+        fileName = `apps/api/app/controllers/${jumpFileName}`; // アクティブにしたいファイルの名前
+      } else {
+        fileName = `apps/api/app/models/${jumpFileName}`; // アクティブにしたいファイルの名前
+      }
 
-    const filePath = vscode.Uri.file(
-      path.join(vscode.workspace.rootPath || "", fileName),
-    );
-    vscode.workspace.openTextDocument(filePath).then((document) => {
-      vscode.window.showTextDocument(document);
-    });
-  });
+      const filePath = vscode.Uri.file(
+        path.join(vscode.workspace.rootPath || "", fileName),
+      );
+      vscode.workspace.openTextDocument(filePath).then((document) => {
+        vscode.window.showTextDocument(document);
+      });
+    },
+  );
 
   let spec_jump = vscode.commands.registerCommand("sjump", () => {
     let editor = vscode.window.activeTextEditor;
@@ -99,17 +126,14 @@ export function activate(context: vscode.ExtensionContext) {
     }
     const currentFilePath = editor.document.fileName;
     const currentFileName = path.basename(currentFilePath);
-    let jumpFileName
-    if(currentFileName.includes("controller")){
+    let jumpFileName;
+    if (currentFileName.includes("controller")) {
       jumpFileName = currentFileName.replace(
         "controller.rb",
         "controller_spec.rb",
       );
-    }else {
-      jumpFileName = currentFileName.replace(
-        ".rb",
-        "_spec.rb",
-      );
+    } else {
+      jumpFileName = currentFileName.replace(".rb", "_spec.rb");
     }
     const fileName = `apps/api/spec/controllers/${jumpFileName}`; // アクティブにしたいファイルの名前
 
@@ -121,9 +145,12 @@ export function activate(context: vscode.ExtensionContext) {
     });
   });
 
-
-
-  context.subscriptions.push(prettier, rspec, controller_or_model_jump, spec_jump);
+  context.subscriptions.push(
+    prettier,
+    rspec,
+    controller_or_model_jump,
+    spec_jump,
+  );
 }
 function convertDoubleToSingleQuotes(input: string): string {
   // ダブルクオーテーションをシングルクオーテーションに置換する
@@ -132,6 +159,34 @@ function convertDoubleToSingleQuotes(input: string): string {
 function convertDoubleToDoubleQuotes(input: string): string {
   // 逆にね
   return input.replace(/'/g, '"');
+}
+
+function getFailContent(
+  message: string,
+  examples: number,
+  success: number,
+  failures: number,
+  filename: string,
+) {
+  return `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>${filename}</title>
+      </head>
+      <body>
+          <h1>ぴえん🥲</h1>
+          <p>実行したテスト => 「 ${examples} 」</p>
+          <p>成功 => 「 ${success} 」</p>
+          <p>失敗 => 「 ${failures} 」</p>
+          <p>メッセージ => 「 ${message} 」</p>
+          <p>「cmd + w」で閉じる</p>
+          <p>「command + shift + [ 」で左のタブに移動</p>
+      </body>
+      </html>
+  `;
 }
 
 export function deactivate() {}
